0fb35fe77311dab22e88624f748ff4a3c85f273d
Pavlo Lytvyshko <pavlo.lytvyshko@jointoit.com>
2026-09-13 13:35:40 +0300

    add +7 enchantment script with improved logic

---
+++ 7.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+:ask_total
+set "total="
+set /p "total=Enter total: "
+if not defined total goto ask_total
+for /f "delims=0123456789" %%A in ("%total%") do goto ask_total
+
+:ask_mirages
+set "mirages="
+set /p "mirages=Enter mirages: "
+if not defined mirages goto ask_mirages
+for /f "delims=0123456789" %%A in ("%mirages%") do goto ask_mirages
+
+cd /d "%~dp0"
+node 7.js "%total%" "%mirages%"
+
+pause
